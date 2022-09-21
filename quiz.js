2c8d5c0a6bbc4af31bee2c6d1a0de385138ebbda
Sudarshanbalichakra <suddu680@gmail.com>
2022-09-21 14:07:39 +0530

    updated

---
+++ quiz.js
@@ -0,0 +1,24 @@
+const quizForm=document.querySelector(".quiz-form");
+const submitButton=document.querySelector("#btn");
+const outputBox=document.querySelector("#output");
+
+const correctAnwser=["90°","right trinagle"];
+
+
+function calculatescore(){
+    let score=0;
+    let index=0;
+   const formResult= new FormData(quizForm);
+   console.log(formResult);
+}
+
+
+
+
+
+
+
+
+
+submitButton.addEventListener("click", calculatescore)
+
